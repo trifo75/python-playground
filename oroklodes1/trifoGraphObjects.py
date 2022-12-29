871662c576c23803a6e13dce57468c3ee747843d
Trifonov András <trifo75@gmail.com>
2022-12-29 09:43:42 +0100

    Első működő verzió

--- oroklodes1/trifoGraphObjects.py
+++ oroklodes1/trifoGraphObjects.py
@@ -20,13 +20,59 @@ class Element():
     randomize_dir = True
 
     # random valtoztatas hatarai
-    max_dir_change = 2
-    max_speed_change = .2
+    max_dir_change = 4
+    max_speed_change = .1
 
-    def chg_dir_random():
+    # legyen basic szine
+    color = (0, 255, 0)
+
+    def chg_dir_random(self):
         """
-        irany megvaltoztatasa veletlenszeruen 
+        irany megvaltoztatasa veletlenszeruen
         max_dir_change erejeig, plusz vagy minusz iranyba
         """
-        self.dir += random.random()*self.max_dir_chanege*2 - self.max_dir_change
+        self.dir += random.random()*self.max_dir_change*2 - self.max_dir_change
 
+    def chg_speed_random(self):
+        """
+        sebesseg megvaltoztatasa veletlenszeruen
+        max_speed_change erejeeig, + vagy - iranyba
+        """
+        self.speed += random.random()*self.max_speed_change*2 - self.max_speed_change
+
+    def move(self):
+        """
+        mozgas egy lepessel sebesseg es irany alapjan
+        ha kell, elotte randomizalunk
+        """
+        if self.randomize_speed :
+            self.chg_speed_random()
+
+        if self.randomize_dir :
+            self.chg_dir_random()
+
+        # váltsuk a fokot radiánba 
+        dir_rad = self.dir / 180 * math.pi
+
+        # számoljuk ki az elmozdulásokat és mozduljunk
+        self.xpos += math.cos(dir_rad) * self.speed
+        self.ypos += math.sin(dir_rad) * self.speed
+        
+class Rectangle(Element):
+    xsize = 20
+    ysize = 20
+
+    def draw(self,my_screen):
+        posx = self.xpos - self.xsize / 2
+        posy = self.ypos - self.ysize / 2
+        pygame.draw.rect(my_screen,self.color,(posx,posy,self.xsize,self.ysize))
+
+class Circle(Element):
+    radius = 10
+
+    def draw(self,my_screen):
+        posx = self.xpos - self.radius
+        posy = self.ypos - self.radius
+        pygame.draw.circle(my_screen,self.color,(posx,posy),self.radius)
+
+        
---
+++ oroklodes1/wandering.py
@@ -0,0 +1,82 @@
+
+import trifoGraphObjects as tgo
+import pygame
+import random
+
+# paraméterek
+# ennyi objektum keletkezzen
+num_elements = 200 
+
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+
+# grafikus elemek feltöltése
+# az objectlist listahoz adogatjuk hozzá véletlenszerűen a
+# köröket vagy négyzeteket. Mindegyik kap random irányt.
+# a random irány lehet, hogy jobb lenne konstruktorként...
+objectlist = []
+for o in range(num_elements):
+
+    if random.getrandbits(1):
+        newobject = tgo.Rectangle()
+        newobject.color = RED
+    else:
+        newobject = tgo.Circle()
+        newobject.color = GREEN
+
+    newobject.dir = random.random() * 360
+    
+    objectlist.append(newobject)
+    
+
+
+
+pygame.init()
+ 
+# Set the width and height of the screen [width, height]
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+ 
+pygame.display.set_caption("My Game")
+ 
+# Loop until the user clicks the close button.
+done = False
+ 
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+ 
+# -------- Main Program Loop -----------
+while not done:
+    # --- Main event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+ 
+    # --- Game logic should go here
+ 
+    # --- Screen-clearing code goes here
+ 
+    # Here, we clear the screen to white. Don't put other drawing commands
+    # above this, or they will be erased with this command.
+ 
+    # If you want a background image, replace this clear with blit'ing the
+    # background image.
+    screen.fill(WHITE)
+ 
+    # --- Drawing code should go here
+    for i, object in enumerate(objectlist):
+        object.move()
+        object.draw(screen)
+
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+ 
+    # --- Limit to 60 frames per second
+    clock.tick(60)
+ 
+# Close the window and quit.
+pygame.quit()
