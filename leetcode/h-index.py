daaed66945b46d4cecc455c3a5483da62ad9fa3d
Trifonov András <trifo75@gmail.com>
2023-01-08 19:23:13 +0100

    h-index számítás kétféleképpen

--- leetcode/h-index.py
+++ leetcode/h-index.py
@@ -1,4 +1,4 @@
-# %%
+# %% h-index rendezetlen és rendezett listán
 
 class Solution:
     def hIndex(self, citations: list[int]) -> int:
@@ -13,12 +13,25 @@ class Solution:
 
         return h
         
+    def hIndex_on_ordered(self, citations: list[int]) -> int:
+        
+        h = 0
+        l = len(citations)
+        while h < citations.pop():
+            h += 1
+        return h
 
 
 
 sample = [3,0,6,1,5]
-sample = [0]
+
+
 s = Solution()
+
+# általános eset
 print(s.hIndex(sample))
 
-# %%
+# rendezett lista esetén
+sample.sort()
+print(s.hIndex_on_ordered(sample))
+
