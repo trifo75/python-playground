2b8f27d7a0e16a495a54e490e3451f502e60ded8
Trifonov András <trifo75@gmail.com>
2023-02-18 08:38:17 +0100

    n.th version, finally working and accepted

--- leetcode/fruit.py
+++ leetcode/fruit.py
@@ -6,8 +6,25 @@ class Solution:
 
     def totalFruit(self, fruits: list[int]) -> int:
 
-        trackbuckets = list()
-        startpositions = list()
+        #preprocessing...
+
+        # empty input string should return 0
+        if len(fruits) == 0: return 0
+        # only one tree should return 1 or 2 fruits
+        if len(fruits) <= 2: return len(fruits)
+
+        # get a list of fruit types and get all possible combinations
+        # of two kinds
+        typelist = list(set(fruits))
+
+        # if we have only one ot two types of fruits, we can
+        # get fruit from all trees
+        if len(typelist) <=2: return len(fruits)
+
+
+        # in all other cases we process stuff
+
+
         buckets = list()
 
         max = 0
@@ -17,38 +34,73 @@ class Solution:
         # * there is a new type of fruit which is NOT in the last 2 types
         # * the "buckets" list have to be trimmed to the last 2 items
         for i,fruit in enumerate(fruits):
-            if not fruit in trackbuckets:
-                trackbuckets.append(fruit)
-                startpositions.append(i)
-                buckets.append(defaultdict(lambda: 0))
-                if len(trackbuckets) > 2:
-                    trackbuckets.pop(0)
-            
-            # trying to enumerate "buckets" and throw out elements 
-            for j, bucket in enumerate(buckets):
-                bucket[fruit] += 1
+            buckets.append(defaultdict(lambda: 0))
 
+            # we increase the num of current fruits in every buckets
+            for j, bucket in reversed(list(enumerate(buckets))):
+                bucket[fruit] +=1
+
+            # we drop last bucket if its contents are aready collected in some other bucket
+            lastbucket = buckets[-1]
+            lastbucketkeys = sorted(list(lastbucket.keys()))
+            for j, bucket in reversed(list(enumerate(buckets[:-1]))):
+                if sorted(list(bucket.keys())) == lastbucketkeys:
+                    buckets.remove(lastbucket)
+                    break
+
+            # if the third type of fruit appears in the bucket ( len > 2 ), then 
+            # collected fruits are counted and the bucket is removed
+            for j, bucket in reversed(list(enumerate(buckets))):
                 if len(bucket) > 2:
-                    curr = sum(list(dict(bucket).values())[:2])
+                    curr = sum(list(bucket.values())[:2]) # sum of the first 2 value in the dict
                     if curr > max: max = curr
+                    buckets.pop(j)
 
-                    # HERE IS THE PROBLEM: popping the item makes the
-                    # next entry to be skipped!
-                    # buckets.pop(j)
-            buckets = list(filter(lambda x:len(x)<=2,buckets))
-            
-        # Handling the buckets remaining after last cycle - if the longest
-        # list is at the end of the list then it is not counted
-
+        # checking remaining buckets
         for i, bucket in enumerate(buckets):
             curr = sum(list(dict(bucket).values())[:2])
             if curr > max: max = curr
+           
 
-        # and returning the result
         return max
 
 
-
+    def totalFruit_best(self, fruits: list[int]) -> int:
+        count=0
+        first=fruits[0]
+        second=None
+        nowmax=0
+        result=0
+        flag=False
+        for i in fruits:
+            if(flag):
+                if(i == second):
+                    count+=1
+                    nowmax+=1
+                elif(i==first):
+                    nowmax+=1
+                    count=1
+                    first=second
+                    second=i
+                else:
+                    if(nowmax>result):
+                        result=nowmax
+                    nowmax=count+1
+                    count=1
+                    first=second
+                    second=i
+            else:
+                if(i==first):
+                    count+=1
+                    nowmax+=1
+                else:
+                    flag=True
+                    count=1
+                    nowmax+=1
+                    second=i
+        if(nowmax>result):
+            result=nowmax
+        return result
 
 
 
@@ -59,6 +111,8 @@ testcases = [
     [0,1,2,2],
     [1,2,3,2,2],
     [0,1,3]*100000 ,
+    [1]*100000,
+    [0,1]*20000,
 ]
 
 
@@ -71,11 +125,14 @@ for c in testcases:
     else:
         print(c)
     print("Output: ",end='')
-    print(s.totalFruit(c))
+    
+    starttime = timeit.default_timer()
+    print(s.totalFruit(c), end=' ')
+    endtime = timeit.default_timer()
+    print("time: {} secs".format(endtime-starttime))
 
 
 #print("TimeIt session")
-#starttime = timeit.default_timer()
 #out = s.totalFruit([0,1]*100000 + [5,6])
 
 #print( "Time diff is ", timeit.default_timer() -starttime )
